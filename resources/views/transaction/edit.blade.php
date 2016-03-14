895d78480ebbfb4e2134f847919c3f3c1fd10371
Robert Horlings <robert@isdat.nl>
2016-03-14 10:20:47 +0100

    Improved transaction edit

--- resources/views/transaction/edit.blade.php
+++ resources/views/transaction/edit.blade.php
@@ -10,7 +10,7 @@
 	'class' => 'form-horizontal' 
 ]) !!}
 <div class='row'>
-	<div class='col-md-8 col-sm-12'>
+	<div class='col-md-7 col-sm-12'>
 
 		<div class="box box-primary">
 			<div class="box-header with-border">
@@ -21,13 +21,13 @@
 				<div
 					class="form-group {{ $errors->has('datum') ? 'has-error' : ''}}">
 					{!! Form::label('statement.omschrijving', 'Description: ', ['class' => 'col-sm-3 control-label']) !!}
-					<div class="col-sm-6">
+					<div class="col-sm-8">
 						{!! Form::text('Statement[omschrijving]', $statement ? $statement->omschrijving : null, ['class' => 'form-control']) !!} 
 					</div>
 				</div>
 				<div class="form-group">
 					{!! Form::label('statement.lines', 'Booked at: ', ['class' => 'col-sm-3 control-label']) !!}
-					<div class="col-sm-6">
+					<div class="col-sm-8">
 						@for($i = 0; $i < $numLines; $i++)
 							{!! Form::hidden('Statement[lines][' . $i . '][id]', $preEnteredLines[$i]['id']) !!} 
 							<div class="row">
@@ -57,7 +57,7 @@
 			
 		</div>
 	</div>
-	<div class='col-md-4 col-sm-12'>
+	<div class='col-md-5 col-sm-12'>
 		<div class="box">
 			<div class="box-header with-border">
 				<h3 class="box-title">Transaction details</h3>
