bcbe486ea38ac0d2eecdc54ce36d10bba101b0e6
Robert Horlings <robert@isdat.nl>
2016-05-04 16:40:35 +0200

    Bugfix for preset value in relation-project-select

--- resources/views/components/selectRelationProject.blade.php
+++ resources/views/components/selectRelationProject.blade.php
@@ -8,8 +8,23 @@
 		if(starts_with($key, 'project'))
 			$relations[$key] = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $title;
 	}
+	
+	// Parse value if given
+	$relatieId = $projectId = null;
+	
+	if($value) {
+		$parts = explode(".", $value);
+		if($parts[0] == "klant" && count($parts) > 1) {
+			$relatieId = $parts[1];
+		}
+		if($parts[0] == "project" && count($parts) > 2) {
+			$relatieId = $parts[1];
+			$projectId = $parts[2];
+		}
+					
+	}
 ?>
 {!! Form::select($name, $relations, $value, $attributes) !!}
 
-{!! Form::hidden('relatie_id', null, [ 'class' => 'hidden-relation-' . $name . '-relatie-id']) !!}
-{!! Form::hidden('project_id', null, [ 'class' => 'hidden-relation-' . $name . '-project-id']) !!}
+{!! Form::hidden('relatie_id', $relatieId, [ 'class' => 'hidden-relation-' . $name . '-relatie-id']) !!}
+{!! Form::hidden('project_id', $projectId, [ 'class' => 'hidden-relation-' . $name . '-project-id']) !!}
